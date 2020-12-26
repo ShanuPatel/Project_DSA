e82b9b45d85d54671329c2ae57637e8ce690d79f
shanu1195 <shanu1195@Trickster.github.com>
2020-12-26 23:56:12 +0530

    TestCase Kth element

--- DSA.cpp
+++ DSA.cpp
@@ -90,27 +90,21 @@ namespace Testcases
 		std::swap(arr[i], arr[r]);
 		return i;
 	}
-	// This function returns k'th smallest element in arr[l..r] using
-	// QuickSort based method.  ASSUMPTION: ALL ELEMENTS IN ARR[] ARE DISTINCT
+
 	int kthSmallest(int arr[], int l, int r, int k)
 	{
 		// If k is smaller than number of elements in array
 		if (k > 0 && k <= r - l + 1) {
-			// Partition the array around last element and get
-			// position of pivot element in sorted array
 			int pos = partition(arr, l, r);
 
 			// If position is same as k
 			if (pos - l == k - 1)
 				return arr[pos];
-			if (pos - l > k - 1) // If position is more, recur for left subarray
+			if (pos - l > k - 1)
 				return kthSmallest(arr, l, pos - 1, k);
 
-			// Else recur for right subarray
 			return kthSmallest(arr, pos + 1, r, k - pos + l - 1);
 		}
-
-		// If k is more than number of elements in array
 		return INT_MAX;
 	}
 
@@ -120,10 +114,6 @@ namespace Testcases
 		*a = *b;
 		*b = temp;
 	}
-
-	// Standard partition process of QuickSort().  It considers the last
-	// element as pivot and moves all smaller element to left of it
-	// and greater elements to right
 
 	void Print()
 	{
